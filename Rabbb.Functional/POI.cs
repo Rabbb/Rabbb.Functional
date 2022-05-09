9b549868b2ed0a18b2d08cb9c6e0329bfc2dc778
Ciaran <CiaranHuang@vip.qq.com>
2022-05-09 10:18:51 +0800

    Add exception type

--- Rabbb.Functional/POI.cs
+++ Rabbb.Functional/POI.cs
@@ -1,9 +1,25 @@
 ﻿namespace Rabbb.Functional
 {
+    /// <summary>
+    /// An function result that has true result when succeed, or false result when failed. <br/>2022-5-9 10:17:17 Ciaran
+    /// </summary>
+    /// <typeparam name="T">True result type.</typeparam>
+    /// <typeparam name="F">False result type.</typeparam>
     public class POI<T , F>
     {
+        /// <summary>
+        /// True result.
+        /// </summary>
         public T? Resolve { get; set; }
 
+        /// <summary>
+        /// False result.
+        /// </summary>
         public F? Reject { get; set; }
+
+        /// <summary>
+        /// Neither True result or False result, it's a program exception. <br/>2022-5-9 10:17:17 Ciaran
+        /// </summary>
+        public Exception? Exception { get; set; }
     }
 }
--- Rabbb.Functional/POIStatic.cs
+++ Rabbb.Functional/POIStatic.cs
@@ -8,14 +8,28 @@ namespace Rabbb.Functional
 {
     public static class POIStatic
     {
+        /// <summary>
+        /// Return a result that is true. <br/>2022-5-9 10:06:55 Ciaran
+        /// </summary>
         public static POI<T, F> True<T, F>(T @true)
         {
             return new POI<T, F>() { Resolve = @true };
         }
 
+        /// <summary>
+        /// Return a result that is false.  <br/>2022-5-9 10:06:55 Ciaran
+        /// </summary>
         public static POI<T, F> False<T, F>(F @false)
         {
             return new POI<T, F>() { Reject = @false };
+        }
+
+        /// <summary>
+        /// Return a result that neither true or false, which is a program exception.  <br/>2022-5-9 10:06:55 Ciaran
+        /// </summary>
+        public static POI<T, F> Except<T, F>(Exception ex)
+        {
+            return new POI<T, F>() { Exception = ex };
         }
     }
 }
