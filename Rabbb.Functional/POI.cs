0f06d25fdfac1de064351b7cbb81f32f49e24c81
Ciaran <CiaranHuang@vip.qq.com>
2022-05-09 09:43:56 +0800

    1.0.1 usable

---
+++ Rabbb.Functional/POI.cs
@@ -0,0 +1,9 @@
+﻿namespace Rabbb.Functional
+{
+    public class POI<T , F>
+    {
+        public T? Resolve { get; set; }
+
+        public F? Reject { get; set; }
+    }
+}
---
+++ Rabbb.Functional/POIStatic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabbb.Functional
+{
+    public static class POIStatic
+    {
+        public static POI<T, F> True<T, F>(T @true)
+        {
+            return new POI<T, F>() { Resolve = @true };
+        }
+
+        public static POI<T, F> False<T, F>(F @false)
+        {
+            return new POI<T, F>() { Reject = @false };
+        }
+    }
+}
